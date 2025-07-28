ce9f6bbc32e873c7d22f12ed44a86477f12af0c4
Alex <heleveins@gmail.com>
2025-07-28 20:21:34 +0200

    segmenter update

--- src/segmentation/segmenter.py
+++ src/segmentation/segmenter.py
@@ -1,32 +1,63 @@
 # src/segmentation/segmenter.py
 from ultralytics import YOLO
 
+# class Segmenter:
+#     def __init__(self, model_path,input_size=1280):
+#         self.model = YOLO(model_path)
+#         self.input_size = input_size
+#         self.class_names = self.model.names
+
+#     def predict(self, img, conf=0.5):
+#         if img is None:
+#             raise ValueError("Image is None")
+
+#         results = self.model.predict(img, conf=conf, verbose=False)[0]
+
+#         masks, boxes, classes, confidences = [], [], [], []
+
+#         if results.masks is not None:
+#             for i, mask in enumerate(results.masks.xy):
+#                 cls_id = int(results.boxes.cls[i].item())
+#                 conf = float(results.boxes.conf[i].item())
+
+#                 masks.append(mask)
+#                 boxes.append(results.boxes.xyxy[i].cpu().numpy())
+#                 classes.append(cls_id)
+#                 confidences.append(conf)
+
+#         return {
+#             "masks": masks,
+#             "boxes": boxes,
+#             "classes": classes,
+#             "confidences": confidences
+#         }
+
 class Segmenter:
-    def __init__(self, model_path,input_size=1280):
+    def __init__(self, model_path, input_size=1280):
         self.model = YOLO(model_path)
         self.input_size = input_size
         self.class_names = self.model.names
 
-    def predict(self, img, conf=0.5):
-        if img is None:
+    def predict(self, image, iou=0.3, conf=0.5, filter_classes=True): # dummy argument for inference class
+        
+        if image is None:
             raise ValueError("Image is None")
 
-        results = self.model.predict(img, conf=conf, verbose=False)[0]
+        results = self.model.predict(image, iou=iou, conf=conf, verbose=False)[0]
 
-        masks, boxes, classes, confidences = [], [], [], []
+        boxes, classes, confidences = [], [], []
 
-        if results.masks is not None:
-            for i, mask in enumerate(results.masks.xy):
+        if results.boxes is not None:
+            for i in range(len(results.boxes)):
                 cls_id = int(results.boxes.cls[i].item())
-                conf = float(results.boxes.conf[i].item())
+                conf_score = float(results.boxes.conf[i].item())
+                box = results.boxes.xyxy[i].cpu().numpy()
 
-                masks.append(mask)
-                boxes.append(results.boxes.xyxy[i].cpu().numpy())
+                boxes.append(box)
                 classes.append(cls_id)
-                confidences.append(conf)
+                confidences.append(conf_score)
 
         return {
-            "masks": masks,
             "boxes": boxes,
             "classes": classes,
             "confidences": confidences
